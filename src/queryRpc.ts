a6e6b1d2e30227dd39690aa62c96b25dd520b0fc
Keshav Gupta <keshav.pg@gmail.com>
2023-12-16 20:13:29 +0100

    FeeCollector ABI and observable creation

--- src/queryRpc.ts
+++ src/queryRpc.ts
@@ -1,6 +1,7 @@
 import { ethers } from 'ethers';
 import { from, Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import FeeCollectorABIJson from './contracts/FeeCollectorABI.json';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -16,21 +17,41 @@ const polygonWsUrl = `wss://polygon-mainnet.infura.io/ws/v3/${INFURA_PROJECT_ID}
 const httpProvider = new ethers.JsonRpcProvider(polygonHttpUrl);
 const wsProvider = new ethers.WebSocketProvider(polygonWsUrl);
 
-// Function to get the latest block number using RxJS
-function getLatestBlockNumber() {
-  return from(wsProvider.getBlockNumber()).pipe(
-    catchError(error => {
-      console.error('Error querying the Polygon Mainnet:', error);
-      throw error;
-    })
-  );
+// Use FeeCollectorABI in your ethers contract setup
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+if (!CONTRACT_ADDRESS) {
+  throw new Error("CONTRACT_ADDRESS environment variable is not set");
+}
+// Parse the ABI string to an object
+const FeeCollectorABI = JSON.parse(FeeCollectorABIJson.result);
+const contract = new ethers.Contract(CONTRACT_ADDRESS, FeeCollectorABI, wsProvider);
+
+// Function to create an Observable that listens for new events
+function feesCollectedObservable() {
+  return new Observable(subscriber => {
+    contract.on("FeesCollected", (_token, _integrator, _integratorFee, _lifiFee, event) => {
+      // Create an object with the event details
+      // The toString() method is called on _integratorFee and _lifiFee because these are likely BigNumber objects
+      const eventData = {
+        token: _token,
+        integrator: _integrator,
+        integratorFee: _integratorFee.toString(),
+        lifiFee: _lifiFee.toString(),
+        blockNumber: event.blockNumber,
+        transactionHash: event.transactionHash
+      };
+
+      // Emit the event data
+      subscriber.next(eventData);
+    });
+
+    // Cleanup logic in case of unsubscription
+    return () => {
+      contract.removeAllListeners("FeesCollected");
+    };
+  });
 }
 
-// Subscribe to the observable to perform the query
-// getLatestBlockNumber().subscribe({
-//   next: blockNumber => console.log(`Latest Polygon Mainnet Block Number: ${blockNumber}`),
-//   error: err => console.error('Error in subscription:', err)
-// });
 
 // Function to create an Observable that listens for new blocks
 function newBlockObservable() {
@@ -55,3 +76,15 @@ const blockSubscription = newBlockObservable().subscribe({
 
 // If you ever need to stop listening for blocks, you can unsubscribe
 // blockSubscription.unsubscribe();
+
+// Subscribe to the observable to listen for new events
+const feesCollectedSubscription = feesCollectedObservable().subscribe({
+  next: event => {
+    console.log(`FeesCollected Event:`, event);
+  },
+  error: err => console.error('Error:', err),
+  complete: () => console.log('Completed')
+});
+
+// To unsubscribe (e.g., when shutting down the application)
+// feesCollectedSubscription.unsubscribe();
